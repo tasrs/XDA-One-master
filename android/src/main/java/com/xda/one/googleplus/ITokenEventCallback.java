39eed7484fc507095afd52ed57829a64ab1c9600
tasrs <sitaram@techaheadcorp.com>
2014-12-17 11:45:59 +0530

    Update gradle  v to 1.0.0

--- android/src/main/java/com/xda/one/googleplus/ITokenEventCallback.java
+++ android/src/main/java/com/xda/one/googleplus/ITokenEventCallback.java
@@ -2,7 +2,9 @@ package com.xda.one.googleplus;
 
 /**
  * Created by akhil on 11/12/14.
+ * get token from google api
  */
 public interface ITokenEventCallback {
+
     public void onTokenResult(String token);
 }
